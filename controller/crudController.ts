ef314e8be0cc91998ee85aafe0edc138eace84e5
gemdevrats <devvrat.sharma@geminisoloution.com>
2022-08-02 18:38:19 +0530

    added xls file access

--- controller/crudController.ts
+++ controller/crudController.ts
@@ -1,7 +1,9 @@
-import { User, userSchema } from "../model/userSchema";
+import { User} from "../model/userSchema";
 import { Request, Response } from "express";
 import jwt from 'jsonwebtoken'
 import { sendMails } from "../email/emailController";
+import exeljs from 'exceljs'
+import path from 'path'
 
 var id: string;
 const readToken = (token: string) => {
@@ -45,11 +47,13 @@ export const updateContact = async (req: Request, res: Response) => {
         const { name, email, mobile } = req.body
         const token: string = req.headers.token as string
         readToken(token)
-        const user = await User.updateOne({'contact._id':req.params.id},{$set:{
-            'contact.$.name':name,
-            'contact.$.email':email,
-            'contact.$.mobile':mobile
-        }})
+        const user = await User.updateOne({ 'contact._id': req.params.id }, {
+            $set: {
+                'contact.$.name': name,
+                'contact.$.email': email,
+                'contact.$.mobile': mobile
+            }
+        })
         res.status(200).send(user)
     } catch (error) {
         console.log(error)
@@ -63,7 +67,7 @@ export const deleteContact = async (req: Request, res: Response) => {
         const token: string = req.headers.token as string
         readToken(token)
         console.log(req.params.id)
-        const user = await User.updateOne({ _id: id }, { $pull: { contact:{ _id: req.params.id } } })
+        const user = await User.updateOne({ _id: id }, { $pull: { contact: { _id: req.params.id } } })
         res.status(200).send(user);
     } catch (error) {
         console.log(error)
@@ -72,16 +76,45 @@ export const deleteContact = async (req: Request, res: Response) => {
 }
 
 
-export const sendMail =async (req: Request, res: Response) => {
+export const sendMail = async (req: Request, res: Response) => {
+    try {
+        const token: string = req.headers.token as string
+        readToken(token)
+        const { to, subject, message } = req.body
+        sendMails(to, subject, message)
+        res.send('cchfc')
+    }
+    catch (error) {
+        console.log(error)
+        res.status(400).send(error)
+    }
+} 
+
+
+export const sheet = async (req: Request, res: Response)=>{
     try{
         const token: string = req.headers.token as string
         readToken(token)
-        const {to, subject, message} = req.body
-        sendMails(to,subject,message)
-        res.send('cchfc')
-    }
-    catch(error){
-        console.log(error)
+        const user = await User.findById(id);
+        const workbook = new exeljs.Workbook();
+        const worksheet = workbook.addWorksheet('Contacts');
+        worksheet.columns = [
+            {header: 'S.no', key: 's_no', width: 10},
+            {header: 'Name', key: 'name', width: 10},
+            {header: 'name', key: 'email', width: 10},
+            {header: 'mobile', key: 'mobile', width: 10}
+        ];
+        let count = 1;
+        user?.contact.forEach(item => {
+            (item as any).s_no = count;
+            worksheet.addRow(item);
+            count += 1;
+        });
+        const data = await workbook.xlsx.writeFile('contacts.xlsx')
+        const filepath = path.join(__dirname,'../contacts.xlsx');
+        console.log(filepath);
+        res.status(200).sendFile(filepath);
+    } catch(error){
         res.status(400).send(error)
     }
 } 
--- routes/crudRoutes.ts
+++ routes/crudRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getContact, createContact, deleteContact, updateContact, sendMail } from '../controller/crudController';
+import { getContact, createContact, deleteContact, updateContact, sendMail,sheet } from '../controller/crudController';
 import { verifyToken } from '../jwt/jwthandler';
 
 export const crudRoutes = express.Router();
@@ -9,3 +9,4 @@ crudRoutes.post('/dashboard',verifyToken,createContact)
 crudRoutes.put('/dashboard/:id',verifyToken,updateContact)
 crudRoutes.delete('/dashboard/:id',verifyToken,deleteContact)
 crudRoutes.post('/sendMail',verifyToken,sendMail)
+crudRoutes.get('/sheet',verifyToken,sheet)
